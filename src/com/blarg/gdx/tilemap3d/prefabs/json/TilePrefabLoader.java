a64683f74ba15fa74ba4152bb4584f1d26e3b175
gered <gered@blarg.ca>
2013-10-14 22:29:26 -0400

    add TilePrefab saving

--- src/com/blarg/gdx/tilemap3d/prefabs/json/TilePrefabLoader.java
+++ src/com/blarg/gdx/tilemap3d/prefabs/json/TilePrefabLoader.java
@@ -34,4 +34,25 @@ public class TilePrefabLoader {
 
 		return prefab;
 	}
+
+	public static void save(TilePrefab prefab, String outputFilename) {
+		if (prefab == null)
+			throw new IllegalArgumentException();
+
+		JsonTilePrefab jsonPrefab = new JsonTilePrefab();
+		jsonPrefab.width = prefab.getWidth();
+		jsonPrefab.height = prefab.getHeight();
+		jsonPrefab.depth = prefab.getDepth();
+
+		byte[] dataBytes = new byte[prefab.getData().length * TileDataSerializer.TILE_SIZE_BYTES];
+		ByteBuffer buffer = ByteBuffer.wrap(dataBytes);
+
+		TileDataSerializer.serialize(prefab, buffer);
+		jsonPrefab.data = new String(Base64Coder.encode(dataBytes));
+
+		Json json = new Json();
+		String output = json.prettyPrint(jsonPrefab);
+		FileHandle outputFile = Gdx.files.local(outputFilename);
+		outputFile.writeString(output, false);
+	}
 }
