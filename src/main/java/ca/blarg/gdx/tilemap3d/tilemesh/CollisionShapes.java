0c61bedc2d65cbe69d4358df55c25540cf05cf42
gered <gered@blarg.ca>
2013-12-31 14:06:41 -0500

    fix creating box shape vertices

--- src/main/java/ca/blarg/gdx/tilemap3d/tilemesh/CollisionShapes.java
+++ src/main/java/ca/blarg/gdx/tilemap3d/tilemesh/CollisionShapes.java
@@ -1,7 +1,9 @@
 package ca.blarg.gdx.tilemap3d.tilemesh;
 
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.utils.ObjectMap;
 
@@ -13,11 +15,52 @@ public class CollisionShapes {
 		shapes.put("cube-1x1x1", buildCube1x1x1());
 	}
 
-	private static Model buildCube1x1x1() {
-		return modelBuilder.createBox(1.0f, 1.0f, 1.0f, null, VertexAttributes.Usage.Position);
-	}
-
 	public static Model get(String shapeName) {
 		return shapes.get(shapeName);
+	}
+
+	private static Model buildCube1x1x1() {
+		modelBuilder.begin();
+		MeshPartBuilder partBuilder = modelBuilder.part("collisionShape", GL10.GL_TRIANGLES, VertexAttributes.Usage.Position, null);
+		addBoxShape(partBuilder, -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f);
+		return modelBuilder.end();
+	}
+
+	private static void addBoxShape(MeshPartBuilder partBuilder, float ax, float ay, float az, float bx, float by, float bz) {
+		// TODO: for some reason, ModelBuilder's createBox() creates a box model in a way that BaseModelTileMesh
+		// collects the vertices from it incorrectly. for now, we just manually create a box's vertices/indices ourself
+
+		partBuilder.vertex(ax, by, bz);  // 0
+		partBuilder.vertex(bx, by, bz);  // 1
+		partBuilder.vertex(ax, by, az);  // 2
+		partBuilder.vertex(bx, by, az);  // 3
+		partBuilder.vertex(bx, ay, bz);  // 4
+		partBuilder.vertex(ax, ay, bz);  // 5
+		partBuilder.vertex(bx, ay, az);  // 6
+		partBuilder.vertex(ax, ay, az);  // 7
+
+		// top
+		partBuilder.index((short)0, (short)1, (short)2);
+		partBuilder.index((short)1, (short)3, (short)2);
+
+		// bottom
+		partBuilder.index((short)4, (short)5, (short)6);
+		partBuilder.index((short)5, (short)7, (short)6);
+
+		// front
+		partBuilder.index((short)6, (short)7, (short)3);
+		partBuilder.index((short)7, (short)2, (short)3);
+
+		// back
+		partBuilder.index((short)5, (short)4, (short)0);
+		partBuilder.index((short)4, (short)1, (short)0);
+
+		// left
+		partBuilder.index((short)7, (short)5, (short)2);
+		partBuilder.index((short)5, (short)0, (short)2);
+
+		// right
+		partBuilder.index((short)4, (short)6, (short)1);
+		partBuilder.index((short)6, (short)3, (short)1);
 	}
 }
